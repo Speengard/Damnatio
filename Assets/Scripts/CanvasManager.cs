b0f5cd5236630d4096a0705676e554f656ecb4e0
RiccardoADA <ricdiste01@gmail.com>
2023-04-27 10:22:09 +0200

    fixed a bug causing the canvas to not be singleton

--- Assets/Scripts/CanvasManager.cs
+++ Assets/Scripts/CanvasManager.cs
@@ -5,8 +5,16 @@ using UnityEngine;
 
 public class CanvasManager : MonoBehaviour
 {
+    public static CanvasManager Instance { get; private set; } 
     private void Awake()
     {
+        if (Instance == null) Instance = this;
+		
+        if (Instance != this) 
+        {
+            Destroy(gameObject);
+            return;
+        }
         DontDestroyOnLoad(this);
     }
 }
