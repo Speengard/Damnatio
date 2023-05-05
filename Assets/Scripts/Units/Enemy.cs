bd63fb9401b2fa762f44401da113d49758d6eea5
RiccardoADA <ricdiste01@gmail.com>
2023-05-05 14:37:05 +0200

    added enemy animations

--- Assets/Scripts/Units/Enemy.cs
+++ Assets/Scripts/Units/Enemy.cs
@@ -10,11 +10,12 @@ public class Enemy : MonoBehaviour
     public int damage; // damage that the enemy does on the player
     public Slider healthSlider;
     [SerializeField] private EnemyHealthController healthController;
+    [SerializeField] private Animator enemyAnimator;
 
     private Animator animator;
     public Transform target;
     [SerializeField] private float speed = 0.3f;
-
+    private Vector3 direction;
     public int ID;
 
 
@@ -23,11 +24,17 @@ public class Enemy : MonoBehaviour
         GameManager.Instance.AddEnemyToList(this);
         target = GameObject.FindGameObjectWithTag("Player").transform;
         healthController.SetupHealthBar(); 
+    
     }
 
-    private void Update()
+    private void FixedUpdate()
     {
-        if (Vector3.Distance(transform.position, target.position) > 0.3f)
+
+        direction = (target.position - transform.position).normalized;
+        enemyAnimator.SetFloat("Horizontal", direction.x);
+        enemyAnimator.SetFloat("Vertical", direction.y);
+
+        if (Vector2.Distance(transform.position, target.position) > 0.3f)
         {
             //move if distance from target is greater than 0.3
             transform.position = Vector2.MoveTowards(transform.position, new Vector2(target.position.x,target.position.y), speed * Time.deltaTime);
