69d86a32ec8c0f71bdc17a65e17bb99e9d084f1c
Serena Palumbo <serenapalumbo22@gmail.com>
2023-05-19 20:11:12 +0200

    Pause menu UI

--- Assets/Scripts/Behaviours/ChangeButtonSprite.cs
+++ Assets/Scripts/Behaviours/ChangeButtonSprite.cs
@@ -5,10 +5,23 @@ using UnityEngine.UI;
 
 public class ChangeButtonSprite : MonoBehaviour
 {
-    [SerializeField] private Sprite newButtonSprite;
+    [SerializeField] private Sprite onButtonSprite;
+    [SerializeField] private Sprite offButtonSprite;
+    [SerializeField] private Sprite onSymbolSprite;
+    [SerializeField] private Sprite offSymbolSprite;
     [SerializeField] private Button button;
+    [SerializeField] private Image symbol;
+    [SerializeField] private bool isOn = true; 
 
     public void ChangeSprite() {
-        button.image.sprite = newButtonSprite;
+        if (isOn) {
+            button.image.sprite = offButtonSprite;
+            symbol.sprite = offSymbolSprite;
+            isOn = false;
+        } else {
+            button.image.sprite = onButtonSprite;
+            symbol.sprite = onSymbolSprite;
+            isOn = true;
+        }
     }
 }
