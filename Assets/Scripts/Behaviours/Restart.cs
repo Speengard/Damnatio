dbb26b79fe6aee08d73a47b74c42563d6a1d8247
RiccardoADA <ricdiste01@gmail.com>
2023-05-22 12:50:00 +0200

    added portal sprites and logic

--- Assets/Scripts/Behaviours/Restart.cs
+++ Assets/Scripts/Behaviours/Restart.cs
@@ -6,7 +6,8 @@ using UnityEngine.SceneManagement;
 
 public class Restart : MonoBehaviour
 {
-    private void OnTriggerEnter2D(Collider2D other)
+    [SerializeField] private Sprite openPortalTexture;
+    private void OnCollisionEnter2D(Collision2D other) {
     {
         if (!other.gameObject.CompareTag("Player")) return;
         if (GameManager.Instance.enemies.Count == 0)
@@ -15,9 +16,26 @@ public class Restart : MonoBehaviour
             LoadRandomScene();
         }
     }
+    }
+    private void OnTriggerEnter2D(Collider2D other) {    
+        {
+            if (!other.gameObject.CompareTag("Player")) return;
+            if (GameManager.Instance.enemies.Count == 0)
+            {;
+                // SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+                LoadRandomScene();
+            }
+        }
+    }
 
 	private void LoadRandomScene() {
 		int index = Random.Range(1, 4);
 		SceneManager.LoadScene(index);
 	}
+
+    private void Update() {
+        if(GameManager.Instance.enemies.Count == 0) {
+            GetComponentInParent<SpriteRenderer>().sprite = openPortalTexture;
+        }
+    }
 }
