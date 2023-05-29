cffca71c0e5cf1951ec0bf068fd2d96d497ff69d
RiccardoADA <ricdiste01@gmail.com>
2023-05-29 19:32:14 +0200

    adjusted weapon damage and player speed

--- Assets/Scripts/Managers/PlayerMovementController.cs
+++ Assets/Scripts/Managers/PlayerMovementController.cs
@@ -20,7 +20,7 @@ public class PlayerMovementController : MonoBehaviour
 
     private Finger MovementFinger = null;
     private Vector2 MovementAmount;
-    private float speedMovement;
+    public float speedMovement;
     [SerializeField] private Animator playerAnimator;
 
     [SerializeField] private GameObject Handle;
