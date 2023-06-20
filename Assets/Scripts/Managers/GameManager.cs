47bf9616eefa6f82e9f8f3b5e0fdd1de2a61d77e
RiccardoADA <ricdiste01@gmail.com>
2023-06-20 11:08:08 +0200

    Update GameManager.cs

--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -67,7 +67,7 @@ public class GameManager : MonoBehaviour
     {
         player = Player.Instance;
         // init the onboarding by enabling the canvas object
-        if (PlayerPrefs.HasKey("isFirstLaunch"))
+        if (!PlayerPrefs.HasKey("isFirstLaunch"))
         {
             onboardingScreen.SetActive(true);
             onboardingScreen.GetComponent<OnboardingManager>().enabled = true;
@@ -172,10 +172,4 @@ public class GameManager : MonoBehaviour
         audioSource.PlayOneShot(churchOST);
         audioSource.loop = true;
     }
-
-    public void loadLevel(){
-
-    }
-
-
 }
